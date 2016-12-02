9eb0ad6df04292789d7cc61504e7450fc4703e47
Ananta <lightway82@gmail.com>
2016-12-02 16:14:01 +0300

    Костяк тестов для ByteHelper

---
+++ javausb/src/main/java/test/org/anantacreative/javausb/USB/ByteHelperTest.java
@@ -0,0 +1,155 @@
+package test.org.anantacreative.javausb.USB; 
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/** 
+* ByteHelper Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>��� 2, 2016</pre> 
+* @version 1.0 
+*/ 
+public class ByteHelperTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: intToByteArray(int value, ByteOrder order) 
+* 
+*/ 
+@Test
+public void testIntToByteArray() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: intTo3ByteArray(int value, ByteOrder order) 
+* 
+*/ 
+@Test
+public void testIntTo3ByteArray() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: intTo2ByteArray(int value, ByteOrder order) 
+* 
+*/ 
+@Test
+public void testIntTo2ByteArray() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: intToByteList(int value, ByteOrder order) 
+* 
+*/ 
+@Test
+public void testIntToByteList() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: intTo3ByteList(int value, ByteOrder order) 
+* 
+*/ 
+@Test
+public void testIntTo3ByteList() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: intTo2ByteList(int value, ByteOrder order) 
+* 
+*/ 
+@Test
+public void testIntTo2ByteList() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: byteArray4ToInt(byte[] val, int startPos, ByteOrder order) 
+* 
+*/ 
+@Test
+public void testByteArray4ToInt() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: byteArray3ToInt(byte[] val, int startPos, ByteOrder order) 
+* 
+*/ 
+@Test
+public void testByteArray3ToInt() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: byteArray2ToInt(byte[] val, int startPos, ByteOrder order) 
+* 
+*/ 
+@Test
+public void testByteArray2ToInt() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: byteArray1ToInt(byte[] val, int startPos) 
+* 
+*/ 
+@Test
+public void testByteArray1ToInt() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: byteArrayToInt(byte[] val, int startPos, int stopPos, ByteOrder order) 
+* 
+*/ 
+@Test
+public void testByteArrayToIntForValStartPosStopPosOrder() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: byteArrayToInt(byte[] val, ByteOrder order) 
+* 
+*/ 
+@Test
+public void testByteArrayToIntForValOrder() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: bytesToHex(byte[] array, int byteInLine, char delmitter) 
+* 
+*/ 
+@Test
+public void testBytesToHex() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
